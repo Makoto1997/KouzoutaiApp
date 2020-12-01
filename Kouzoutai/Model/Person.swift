ceac0b1c5aa3d0e5240624432433631e30198161
Makoto <>
2020-12-01 21:35:58 +0900

    First Commit

---
+++ Kouzoutai/Model/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  Kouzoutai
+//
+//  Created by Makoto on 2020/12/01.
+//
+
+import Foundation
+
+struct Person {
+    
+    var name = String()
+    var shumi = String()
+    var eat = String()
+    
+}
